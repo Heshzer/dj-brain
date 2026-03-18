10f3b70ea660fadce8f1bae5871b72abef30cd1e
Helsayed <hanny.elsayed@yahoo.fr>
2026-03-18 23:46:16 +0100

    testy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          // Vercel Edge Proxy n'accepte QUE les ports standards (80 ou 443).
-          // Le routeur de l'ami devra rediriger le port 80 externe vers son port 4000 interne.
-          destination: `http://marcib.ddns.net/api/:path*`,
-        },
-      ]
-    },
-  }
-  
-  export default nextConfig
-=======
       // Proxy manuel via /api/[...proxy]/route.ts pour contourner le blocage Vercel
 }
 export default nextConfig
->>>>>>> 1351584d9af0da47f8624639f0eaef0af43923ac
